428a307100ac1ef4502d19aee5d71a7e0afb3afe
Jacopo Sciacca <Jacopo.Sciacca@hgn.it>
2018-02-05 12:11:06 +0100

    Added the logic to display the list of authors and books

---
+++ src/main/java/guru/org/springframework/controllers/BookController.java
@@ -0,0 +1,24 @@
+package guru.org.springframework.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.org.springframework.repositories.BookRepository;
+
+@Controller
+public class BookController {
+
+	private BookRepository bookRepository;
+	
+	public BookController(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	
+	@RequestMapping("/books")
+	public String books(Model model){
+		model.addAttribute("books", bookRepository.findAll());
+		return "books";
+	}
+}
