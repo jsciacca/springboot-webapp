c7df83cbc717ad6d6be987a896b8b0f64c55b826
Jacopo Sciacca <Jacopo.Sciacca@hgn.it>
2018-02-05 10:36:02 +0100

    added Publisher class and PublisherRepository class

---
+++ src/main/java/guru/org/springframework/repositories/PublisherRepository.java
@@ -0,0 +1,9 @@
+package guru.org.springframework.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.org.springframework.model.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+
+}
